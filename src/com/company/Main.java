e54ef1c392e92481eea62965fb41f48e3d0a2821
vlad0 <sticc.pge@gmail.com>
2022-03-05 13:53:13 +0200

    initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("kvo staa bebka?");
+        System.out.println("¯\\_(ツ)_/¯");
     }
 }
